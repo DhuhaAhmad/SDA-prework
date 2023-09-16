4e7102d70c560d51ec3bb2957e29213a59f96097
DhuhaAhamd <dhuha9712@gmail.com>
2023-09-16 17:56:57 +0300

    done

--- JavaScript/Calculator.js
+++ JavaScript/Calculator.js
@@ -1,1 +1,22 @@
 // Javascript
+const dispaly = document.querySelector(".display")
+
+const getIntoDispaly = (value) =>{
+    dispaly.value+=value  
+}
+const Calcu = () =>{
+    const d = dispaly.value
+    console.log(d)
+    dispaly.value = eval(dispaly.value)
+}
+
+
+const cleardispaly = () =>{
+    dispaly.value=""
+}
+
+
+
+
+
+
